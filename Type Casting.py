ad42c734b89ef957c78c9cb110757d78091b6721
Subham <subhamswain=181@gmail.com>
2025-10-19 11:41:40 +0530

    adding new type casting

---
+++ Type Casting.py
@@ -0,0 +1,29 @@
+# type casting + = the process of converting a variable form one data type to another
+                    # str(), int () , float (),  bool()
+
+name = "subham swain "      # string
+age = 19                     #int
+cgpa = 8.05                  #float
+is_student = True            #bollean
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
+
+age += 1
+print(age)
+age = bool(age)
+print (age)
+
+
+name = str(name)
+print(name)
+
+
+cgpa = bool(cgpa)
+print(cgpa)
+
+
+is_student = bool(is_student)
+print(is_student)
