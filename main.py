ad289c75a3fc6aea29c76696882ba1d5f4ce4311
Subham <subhamswain=181@gmail.com>
2025-10-19 11:15:24 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#integer
+age = 20
+print (age)
+print(f"my age is {age} for my style")
+
+# string
+name = " subham"
+print(name)
+print(f"my name is {name}")
+
+# float
+price = 10.99
+cgpa = 3.2
+distance = 2.00
+print(f"this price is ${price}")
+print(f" my gpa is {cgpa}")
+print(f"my distance is {distance}")
+
+# boolean
+is_student = True
+is_student = False
+print(f" are you a student ?{is_student}")
+if is_student:
+    print("you are a student")
+else:
+    print("you are not a student")
+
+for_sale = False
+if for_sale:
+    print("you are a saller")
+else:
+    print("you are not a saller")
+
+
+is_online = False
+if is_online:
+    print("you are online")
+else:
+    print("you are offline")
