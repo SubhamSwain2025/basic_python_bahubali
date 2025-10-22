ac9f2e99c9745512404fe32ac6d0f041e906a8e8
Subham <subhamswain=181@gmail.com>
2025-10-22 19:36:37 +0530

    adding new math learnings

---
+++ Madlibs game/madlis_game.py
@@ -0,0 +1,10 @@
+# madis game
+# word game  where we create a story
+# by fill in the blanks with random words
+adjective1 = input("what is your foubourite place ?: ")
+place1 = input ("what you shaw there ?:")
+happy1 = input("who are going with you ? : ")
+print(f"Today i will go to  {adjective1} zoo")
+print(f" i am shocked i was saw a {place1} i will scared")
+print(f"     i will happy to go to the {adjective1} and i will saw the {place1}with my family {happy1}to going ")
+print(f"thanks to my parents {happy1}")
